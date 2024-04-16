10fe228a8f50d56d9067f1da8e85921c0df1e1aa
Ramin Azim <127709108+raminazim@users.noreply.github.com>
2024-04-16 13:24:27 -0400

    Commit

--- Assignment2/Python2Q4_DigitSepRA/main.py
+++ Assignment2/Python2Q4_DigitSepRA/main.py
@@ -3,9 +3,9 @@
 #Title: Digit Seperate
 #Purpose: Seperate Digits
 
-num = int(input('Enter 2 digit number: '))
+Num = int(input('Enter 2 digit number: '))
 
-ten = num // 10
-unit = num % 10
+Ten = Num // 10
+Unit = Num % 10
 
-print(f'ten: {ten} \n unit: {unit}')
+print(f'ten: {Ten} \n unit: {Unit}')
