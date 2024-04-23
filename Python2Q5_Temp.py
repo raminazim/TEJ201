d8c796829a8b8089d32698d0de59b546c6cce2fd
Ramin Azim <127709108+raminazim@users.noreply.github.com>
2024-04-22 23:34:05 -0400

    Commit

--- Python2Q5_Temp.py
+++ Python2Q5_Temp.py
@@ -5,22 +5,17 @@
 
 change = int(input("Change: "))
 
-# Calculate the number of quarters
 quarters = change // 25
 remainingCents = change % 25
 
-# Calculate the number of dimes
 dimes = remainingCents // 10
 remainingCents %= 10
 
-# Calculate the number of nickels
 nickels = remainingCents // 5
 remainingCents %= 5
 
-# The remaining cents will be the number of pennies
 pennies = remainingCents
 
-# Output the result
 print("Quarters:", quarters)
 print("Dimes:", dimes)
 print("Nickels:", nickels)
