3657444979b06ad413edc6d48f74d82b214a1674
Ramin Azim <127709108+raminazim@users.noreply.github.com>
2024-05-09 09:33:06 -0400

    Update Q4.py

--- Python4/Q4.py
+++ Python4/Q4.py
@@ -1,1 +1,16 @@
+Combo1 = 28
+Combo2 = 8
+Combo3 = 5
 
+print('Lock has been locked, unlock it.')
+
+while True:
+    Num1 = int(input('Number 1: '))
+    Num2 = int(input('Number 2: '))
+    Num3 = int(input('Number 3: '))
+
+    if Num1 == Combo1 and Num2 == Combo2 and Num3 == Combo3:
+        print("Unlocked")
+        break
+    else:
+        print("Incorrect")
