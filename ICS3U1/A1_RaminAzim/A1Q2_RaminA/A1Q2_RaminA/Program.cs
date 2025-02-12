32e5b7bc5419ff80c8d27bc8283a7ad0620dbbe8
Ramin Azim <127709108+raminazim@users.noreply.github.com>
2025-02-11 21:30:50 -0500

    commit

---
+++ ICS3U1/A1_RaminAzim/A1Q2_RaminA/A1Q2_RaminA/Program.cs
@@ -0,0 +1,394 @@
+﻿/*
+ * Name: Ramin
+ * Date: 02/07/25
+ * Title: ASCII Porche
+ * Purpose: To Display A Porsche 911 Turbo S In ASCII Art
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A1Q2_RaminA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Line 1
+            Console.Write("                                                                      .................                                                                                       \n");
+
+            //Line 2
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("                                                          @");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("..................................... \n");
+
+            //Line 3
+            Console.Write("                                                  ....");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("..........");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@@@@@@@@@@@@@@@@@@@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("...");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write(".. \n");
+
+            //Line 4
+            Console.Write("                                           ....");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write(".......");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@@@@@@@@@@@@@@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("..........");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("..");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("... \n");
+
+            //Line 5
+            Console.Write("                                    ......");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("........");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@@@@@@@@@@@@@@@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("..................");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("..");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("....... \n");
+
+            //Line 6
+            Console.Write("                                ..............");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@@@@@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write(".......");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("......................");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("..");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("....... \n");
+
+            //Line 7
+            Console.Write("            ...");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("          @@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write(".................");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("............");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("............................");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write(".");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("........ \n");
+
+            //Line 8
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("            @@@@@@@@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write(".........");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@@@@@@@@@@@@@@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write(".");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@@@@@@@@@@@@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write(".......................");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@@@@@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write(".");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write(".");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("...... \n");
+
+            //Line 9
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("             @@@@@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("...............................");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("..................................");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@@@@@@@@@@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write(".");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@@@@@@@@@@@@@@@@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write(". \n");
+
+            //Line 10
+            Console.Write("           .....");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("..................................");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write(".........................................");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("......................");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@@@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("......... \n");
+
+            //Line 11
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("           @@@@@@@@@@@@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("...........");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@@@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write(".......");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write(".....................................");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("............................................");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("@@@@@ \n");
+
+            //Line 12
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("         .");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("@@@@@@@@@@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("..........");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@@@@@@@@@@@@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("...");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("....................................................................");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@@@@@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write(".........");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write(".");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("@@@ \n");
+
+            //Line 13
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("       ........................");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@@@@@@@@@@@@@@@@@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("...");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("...........");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("............");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@@@@@@@@@@@@@@@@@@@@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("..");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write(".........");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@@@@@@@@@@@@@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write(".......");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("... \n");
+
+            //Line 14
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("        @");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("..");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@@@@@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("........");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@@@@@@@@@@@@@@@@@@@@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("..");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@@");
+            Console.Write("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("......");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@@@@@@@@@@@@@@@@@@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write(".......");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("..... \n");
+
+            //Line 15
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("        @");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write(".");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@@@@@@@@@@@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write(".");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write(".......");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("....");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("..........");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("..... \n");
+
+            //Line 16
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("        @");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("....");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("....");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("...");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("............... \n");
+
+            //Line 17
+            Console.Write("        .");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("..");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("..");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@@@@@@@ \n");
+
+            //Line 18
+            Console.Write("       @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ \n");
+
+            //Line 19
+            Console.Write("       @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write(". \n");
+
+            //Line 20
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("       @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ \n");
+
+            //Line 21
+            Console.Write("        @@@@");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write(".");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write(".................");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("... \n");
+
+            //Line 22
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("           @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ \n");
+
+            //Line 23
+            Console.Write("                                @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ \n");
+
+            //Line 24
+            Console.Write("                                  @@@@@@@@@@@@@@@@@@@                                                                       @@@@@@@@@@@@@@@@@@@ \n");
+
+            //Line 25
+            Console.Write("                                    @@@@@@@@@@@@@@                                                                            @@@@@@@@@@@@@@ \n");
+
+            //Line 26
+            Console.Write("                                        @@@@@@@                                                                                  @@@@@@@@ \n");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ICS3U1/A1_RaminAzim/A1Q3_RaminA/A1Q3_RaminA/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Name: Ramin
+ * Date: 02/07/25
+ * Title: Poem
+ * Display My Poem (I'm A Poetic Genius)
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A1Q3_RaminA
+{
+    class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(
+        "                   The Rat Race - By Ramin\n\n" +
+        "                  Run, run, faster each day,\n" +
+        "               Climbing ladders, no time to stay.\n" +
+        "                Chasing gold, yet losing sight,\n" +
+        "              Trading dreams for sleepless nights.\n\n" +
+        "                Tick, tock, the clock won’t wait,\n" +
+        "              Bound by deadlines, sealed by fate.\n" +
+        "                 Footsteps echo, pavement worn,\n" +
+        "               Hearts are heavy, souls are torn.\n\n" +
+        "                 A finish line that moves ahead,\n" +
+        "               A life half-lived, a path we tread.\n" +
+        "               Slow down, breathe, break the chain,\n" +
+        "                 Find the sun beyond the rain.\n\n\n"
+        );
+
+        Console.ReadKey();
+        }
+}
+}
